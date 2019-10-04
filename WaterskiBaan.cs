a98d99300f20069b3cafd5e9b3be9c9e4cae0c46
theredghostwolf <jort-hoogerhuis@home.nl>
2019-10-04 13:11:15 +0200

    opdracht 4

---
+++ WaterskiBaan.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace WaterskiBaan
+{
+    public class WaterskiBaan
+    {
+        private LijnenVoorraad voorraad;
+        private Kabel kabel;
+
+        private static int startSupply = 15;
+
+        public WaterskiBaan()
+        {
+            voorraad = new LijnenVoorraad();
+            kabel = new Kabel();
+
+            for (int i = 0; i < startSupply; i++)
+             {
+                voorraad.LijnToevoegenAanRij(new Lijn());
+            }
+        }
+
+        public void VerplaatsKabel ()
+        {
+            kabel.VerschuifLijnen();
+            voorraad.LijnToevoegenAanRij(kabel.verwijderLijnVanKabel());
+        }
+
+        public override string ToString()
+        {
+            return voorraad.ToString() + " \n " + kabel.ToString();
+        }
+    }
+}
